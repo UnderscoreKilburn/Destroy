4bbbe6af0812604fa47fadf6e8ca1843bb8a8cd0
UnderscoreKilburn <trtv@live.fr>
2025-06-27 04:53:43 +0200

    🌈 Mixin it up
    - Fixed Mixtures seen through transparent pipes not updating when the color of their source changes
    - Multiple Mixtures can now travel down the same pipe network and will visually mix when seen through transparent pipes
    - Supplementaries compat: Faucets now properly transfer Mixtures and voiding Mixtures using a sponge now pollutes the world accordingly

--- src/main/java/com/petrolpark/destroy/mixin/PipeConnectionMixin.java
+++ src/main/java/com/petrolpark/destroy/mixin/PipeConnectionMixin.java
@@ -5,23 +5,16 @@ import java.util.function.Predicate;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.petrolpark.destroy.Destroy;
-import com.petrolpark.destroy.chemistry.legacy.LegacyMixture;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import com.petrolpark.destroy.DestroyFluids;
 import com.petrolpark.destroy.mixin.accessor.FluidNetworkAccessor;
 import com.petrolpark.destroy.mixin.accessor.PipeConnectionAccessor;
-import com.simibubi.create.content.fluids.FlowSource;
 import com.simibubi.create.content.fluids.FluidNetwork;
 import com.simibubi.create.content.fluids.PipeConnection;
-import com.simibubi.create.content.fluids.PipeConnection.Flow;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
@@ -29,15 +22,30 @@ import net.minecraftforge.fluids.FluidStack;
 
 @Mixin(PipeConnection.class)
 public class PipeConnectionMixin {
-    static private ThreadLocal<Optional<FluidNetwork>> retainedNetwork = new ThreadLocal<>();
+    private ThreadLocal<Optional<FluidNetwork>> retainedNetwork = new ThreadLocal<>();
+    private ThreadLocal<Boolean> shouldUpdateData = new ThreadLocal<>();
 
     @Inject(
-        method="Lcom/simibubi/create/content/fluids/PipeConnection;manageFlows(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraftforge/fluids/FluidStack;Ljava/util/function/Predicate;)Z",
+        method= "manageFlows(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraftforge/fluids/FluidStack;Ljava/util/function/Predicate;)Z",
         at=@At("HEAD"),
         remap=false
     )
-    private void onStartManageFlows(Level world, BlockPos pos, FluidStack internalFluid, Predicate<FluidStack> extractionPredicate, CallbackInfoReturnable<Boolean> cir) {
+    private void manageFlows_onStart(Level world, BlockPos pos, FluidStack internalFluid, Predicate<FluidStack> extractionPredicate, CallbackInfoReturnable<Boolean> cir) {
         retainedNetwork.set(((PipeConnectionAccessor)this).getNetwork());
+        shouldUpdateData.set(false);
+    }
+
+    @Inject(
+        method= "manageFlows(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraftforge/fluids/FluidStack;Ljava/util/function/Predicate;)Z",
+        at=@At("RETURN"),
+        cancellable = true,
+        remap=false
+    )
+    private void manageFlows_onReturn(Level world, BlockPos pos, FluidStack internalFluid, Predicate<FluidStack> extractionPredicate, CallbackInfoReturnable<Boolean> cir) {
+        // Returning true from PipeConnection.manageFlows causes its data to be sent from the server to the client.
+        // We want that to happen whenever the Mixture carried by this connection changes.
+        if(shouldUpdateData.get())
+            cir.setReturnValue(true);
     }
 
     /**
@@ -46,12 +54,12 @@ public class PipeConnectionMixin {
      * these change a lot. We don't want to try restarting the flow every tick.
      */
     @WrapOperation(
-            method="Lcom/simibubi/create/content/fluids/PipeConnection;manageFlows(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraftforge/fluids/FluidStack;Ljava/util/function/Predicate;)Z",
-            at=@At(
-                    value="INVOKE",
-                    target="Lnet/minecraftforge/fluids/FluidStack;isFluidEqual(Lnet/minecraftforge/fluids/FluidStack;)Z"
-            ),
-            remap=false
+        method= "manageFlows(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraftforge/fluids/FluidStack;Ljava/util/function/Predicate;)Z",
+        at=@At(
+            value="INVOKE",
+            target="Lnet/minecraftforge/fluids/FluidStack;isFluidEqual(Lnet/minecraftforge/fluids/FluidStack;)Z"
+        ),
+        remap=false
     )
     private boolean considerMixturesEqual(FluidStack fluid, FluidStack other, Operation<Boolean> original) {
         if(original.call(fluid, other))
@@ -59,7 +67,8 @@ public class PipeConnectionMixin {
         else if(DestroyFluids.isMixture(fluid) && DestroyFluids.isMixture(other)) {
             ((PipeConnectionAccessor)this).getFlow().get().fluid = fluid;
             if(retainedNetwork.get().isPresent())
-                ((FluidNetworkAccessor)(retainedNetwork.get()).get()).setFluid(fluid);
+                ((FluidNetworkAccessor)retainedNetwork.get().get()).setFluid(fluid);
+            shouldUpdateData.set(true);
             return true;
         }
 

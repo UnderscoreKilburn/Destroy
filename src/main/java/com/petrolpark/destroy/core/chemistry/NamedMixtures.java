431430bc02b331a73d421edd31fd462868be6657
UnderscoreKilburn <trtv@live.fr>
2026-03-15 06:30:37 +0100

    📋 Ordered molecules for real this time

--- src/main/java/com/petrolpark/destroy/core/chemistry/NamedMixtures.java
+++ src/main/java/com/petrolpark/destroy/core/chemistry/NamedMixtures.java
@@ -11,20 +11,20 @@ import com.simibubi.create.foundation.fluid.FluidHelper;
 import net.minecraftforge.common.util.Lazy;
 import net.minecraftforge.fluids.FluidStack;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public enum NamedMixtures {
-    VODKA(() -> mixByVolume(Map.of(DestroyMolecules.ETHANOL, 400.0, DestroyMolecules.WATER, 600.0))),
-    ETHANOL_DISTILLATE(() -> mixByVolume(Map.of(DestroyMolecules.ETHANOL, 300.0, DestroyMolecules.ACETIC_ACID, 30.0, DestroyMolecules.WATER, 670.0))),
+    VODKA(() -> mixByVolume(entryList(DestroyMolecules.ETHANOL, 400.0, DestroyMolecules.WATER, 600.0))),
+    ETHANOL_DISTILLATE(() -> mixByVolume(entryList(DestroyMolecules.ETHANOL, 300.0, DestroyMolecules.ACETIC_ACID, 30.0, DestroyMolecules.WATER, 670.0))),
     LATEX(() -> pure(DestroyMolecules.ISOPRENE)),
     DISTILLED_WATER(() -> pure(DestroyMolecules.WATER)),
 
-    AMMONIA_SOLUTION(null, () -> mixByVolume(Map.of(DestroyMolecules.AMMONIA, 20.0, DestroyMolecules.WATER, 980.0))),
+    AMMONIA_SOLUTION(null, () -> mixByVolume(entryList(DestroyMolecules.AMMONIA, 20.0, DestroyMolecules.WATER, 980.0))),
 
-    BORAX_SOLUTION(() -> solution(DestroyMolecules.WATER, 500, Map.of(
+    BORAX_SOLUTION(() -> solution(DestroyMolecules.WATER, 500, entryList(
         DestroyMolecules.BORIC_ACID, 0.16969298,
         DestroyMolecules.TETRAHYDROXYBORATE, 0.1201766,
         DestroyMolecules.SODIUM_ION, 0.15305416,
@@ -34,25 +34,25 @@ public enum NamedMixtures {
         DestroyMolecules.CHLORIDE, 0.7818937
     ))),
 
-    BRINE(() -> solution(DestroyMolecules.WATER, 500, Map.of(DestroyMolecules.SODIUM_ION, 1., DestroyMolecules.CHLORIDE, 1.))),
+    BRINE(() -> solution(DestroyMolecules.WATER, 500, entryList(DestroyMolecules.SODIUM_ION, 1., DestroyMolecules.CHLORIDE, 1.))),
     MOLTEN_SULFUR(() -> pure(DestroyMolecules.OCTASULFUR).setTemperature(388)),
 
-    MIXED_XYLENES(() -> mixByConcentration(Map.of(
+    MIXED_XYLENES(() -> mixByConcentration(entryList(
         DestroyMolecules.PARAXYLENE, 1.,
         DestroyMolecules.METAXYLENE, 1.,
         DestroyMolecules.ORTHOXYLENE, 1.,
         DestroyMolecules.ETHYLBENZENE, 1.
     ))),
 
-    REFINERY_GAS(() -> pressurize(mixByConcentration(Map.of(
+    REFINERY_GAS(() -> pressurize(mixByConcentration(entryList(
         DestroyMolecules.METHANE, 1.,
         DestroyMolecules.ETHENE, 3.,
         DestroyMolecules.PROPENE, 2.,
         DestroyMolecules.BUTADIENE, 2.
     )), 1250000)),
 
-    SODIUM_AMALGAM(() -> solution(DestroyMolecules.MERCURY, 250, Map.of(DestroyMolecules.SODIUM_METAL, 0.9))),
-    CHLORINE_SOLUTION(() -> solution(DestroyMolecules.WATER, 250, Map.of(DestroyMolecules.HYDROCHLORIC_ACID, 0.92, DestroyMolecules.HYPOCHLOROUS_ACID, 0.92)))
+    SODIUM_AMALGAM(() -> solution(DestroyMolecules.MERCURY, 250, entryList(DestroyMolecules.SODIUM_METAL, 0.9))),
+    CHLORINE_SOLUTION(() -> solution(DestroyMolecules.WATER, 250, entryList(DestroyMolecules.HYDROCHLORIC_ACID, 0.92, DestroyMolecules.HYPOCHLOROUS_ACID, 0.92)))
     ;
 
     private Lazy<FluidStack> fluid;
@@ -68,14 +68,14 @@ public enum NamedMixtures {
         return FluidHelper.copyStackWithAmount(fluid.get(), amount);
     }
 
-    public static LegacyMixture solution(LegacySpecies solvent, int totalVolume, Map<LegacySpecies, Double> contents) {
+    public static LegacyMixture solution(LegacySpecies solvent, int totalVolume, List<Map.Entry<LegacySpecies, Double>> contents) {
         LegacyMixture mixture = new LegacyMixture();
-        contents.forEach((sp, c) -> mixture.addMolecule(sp, c.floatValue()));
+        contents.forEach(e -> mixture.addMolecule(e.getKey(), e.getValue().floatValue()));
 
         double totalVolumeInLiters = (double)totalVolume / Constants.MILLIBUCKETS_PER_LITER;
         double solventVolumeInLiters = totalVolumeInLiters;
 
-        for (Map.Entry<LegacySpecies, Double> entry : contents.entrySet()) {
+        for (Map.Entry<LegacySpecies, Double> entry : contents) {
             LegacySpecies molecule = entry.getKey();
             double molesOfMolecule = entry.getValue() * totalVolumeInLiters;
             solventVolumeInLiters -= molesOfMolecule / molecule.getPureConcentration();
@@ -89,15 +89,15 @@ public enum NamedMixtures {
         return LegacyMixture.pure(molecule).setTemperature(298f);
     }
 
-    public static LegacyMixture mixByConcentration(Map<LegacySpecies, Double> contents) {
+    public static LegacyMixture mixByConcentration(List<Map.Entry<LegacySpecies, Double>> contents) {
         LegacyMixture mixture = new LegacyMixture();
-        contents.forEach((sp, c) -> mixture.addMolecule(sp, c.floatValue()));
+        contents.forEach(e -> mixture.addMolecule(e.getKey(), e.getValue().floatValue()));
         mixture.recalculateVolume(1000);
         return mixture;
     }
 
-    public static LegacyMixture mixByVolume(Map<LegacySpecies, Double> contents) {
-        return LegacyMixture.mix(contents.entrySet().stream().collect(Collectors.toMap(
+    public static LegacyMixture mixByVolume(List<Map.Entry<LegacySpecies, Double>> contents) {
+        return LegacyMixture.mix(contents.stream().collect(Collectors.toMap(
             e -> LegacyMixture.pure(e.getKey()),
             e -> e.getValue()
         ))).setTemperature(298f);
@@ -107,5 +107,30 @@ public enum NamedMixtures {
         float currentPressure = LegacyReaction.GAS_CONSTANT * 1000f * mixture.getTemperature() * mixture.getTotalConcentration();
         mixture.scale(currentPressure / targetPressure);
         return mixture;
+    }
+
+    private static <K,V> List<Map.Entry<K, V>> entryList(K k1, V v1) {
+        return List.of(Map.entry(k1, v1));
+    }
+    private static <K,V> List<Map.Entry<K, V>> entryList(K k1, V v1, K k2, V v2) {
+        return List.of(Map.entry(k1, v1), Map.entry(k2, v2));
+    }
+    private static <K,V> List<Map.Entry<K, V>> entryList(K k1, V v1, K k2, V v2, K k3, V v3) {
+        return List.of(Map.entry(k1, v1), Map.entry(k2, v2), Map.entry(k3, v3));
+    }
+    private static <K,V> List<Map.Entry<K, V>> entryList(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4) {
+        return List.of(Map.entry(k1, v1), Map.entry(k2, v2), Map.entry(k3, v3), Map.entry(k4, v4));
+    }
+    private static <K,V> List<Map.Entry<K, V>> entryList(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5) {
+        return List.of(Map.entry(k1, v1), Map.entry(k2, v2), Map.entry(k3, v3), Map.entry(k4, v4), Map.entry(k5, v5));
+    }
+    private static <K,V> List<Map.Entry<K, V>> entryList(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6) {
+        return List.of(Map.entry(k1, v1), Map.entry(k2, v2), Map.entry(k3, v3), Map.entry(k4, v4), Map.entry(k5, v5), Map.entry(k6, v6));
+    }
+    private static <K,V> List<Map.Entry<K, V>> entryList(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7) {
+        return List.of(Map.entry(k1, v1), Map.entry(k2, v2), Map.entry(k3, v3), Map.entry(k4, v4), Map.entry(k5, v5), Map.entry(k6, v6), Map.entry(k7, v7));
+    }
+    private static <K,V> List<Map.Entry<K, V>> entryList(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7, K k8, V v8) {
+        return List.of(Map.entry(k1, v1), Map.entry(k2, v2), Map.entry(k3, v3), Map.entry(k4, v4), Map.entry(k5, v5), Map.entry(k6, v6), Map.entry(k7, v7), Map.entry(k8, v8));
     }
 }
